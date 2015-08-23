c59edf65bb3e86a835c4e942e7eeaaf5be98319f
Aleksander Murin <a@murin.ru>
2015-08-23 17:50:59 +0700

    Fix error when collection does not exists

--- index.js
+++ index.js
@@ -12,35 +12,48 @@ co(function* () {
 	const url = 'mongodb://localhost/test?w=1&journal=false';
 	const col = 'test';
 
+	console.log('Connecting');
 	var db = yield mongodb.MongoClient.connect(url, {promiseLibrary: rsvp.Promise});
-	yield db.dropCollection(col);
+
+	console.log('Make collection');
+	yield db.dropCollection(col).catch(function () {});
 	var test = yield db.createCollection(col);
 
+	console.log('Make indexes');
 	test.createIndex({hash: 1, ts: 1});
+	test.createIndex({hash: 1, count: 1});
 	test.createIndex({expires: 1}, {expireAfterSeconds: 0});
 
 	var hash = '123';
 	var ts = Math.trunc(Date.now()/1000);
 
+	console.log('Make updates');
 	var promises = new Array(20)
-		.join(',')
-		.split(',')
-		.map(function () {
+			.join(',')
+			.split(',')
+			.map(function () {
 
-			return test.findOneAndUpdate(
-				{hash: hash, ts: ts},
-				{
-					$set: {expires: new Date(Date.now() + 60 * 1000)},
-					$inc: {count: 1}
-				},
-				{upsert: true}
-			);
-		});
+				return test.findOneAndUpdate(
+					{hash: hash, ts: ts},
+					{
+						$set: {expires: new Date(Date.now() + 60 * 1000)},
+						$inc: {count: 1}
+					},
+					{upsert: true}
+				);
+			});
 
+	console.time('update');
 	yield promises;
+	console.timeEnd('update');
 
 	var docs = yield test.find({}, {_id: 0}).toArray();
 	console.log(docs);
 
 	process.exit();
-});
+})
+	.catch(function (err) {
+
+		console.error(err);
+		process.exit();
+	});
--- mongodb.sh
+++ mongodb.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mongod --config mongod.conf
+mongod --config mongod.conf &
