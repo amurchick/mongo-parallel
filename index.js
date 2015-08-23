99a518784fb4f15124a6eb61bd135d0ef1d700de
Aleksander Murin <a@murin.ru>
2015-08-23 16:26:25 +0700

    Ready to run test application
    Simulate one moment write from 20 host to one db

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+/**
+	* Created by a.murin on 23/08/15.
+	*/
+
+var mongodb = require('mongodb');
+var co = require('co');
+var rsvp = require('rsvp');
+
+co(function* () {
+
+	const url = 'mongodb://localhost/test?w=1&journal=false';
+	const col = 'test';
+	const indexFields = [{hash: 1, ts: 1}, {expires: 1}];
+
+	var db = yield mongodb.MongoClient.connect(url, {promiseLibrary: rsvp.Promise});
+	yield db.dropCollection(col);
+	var test = yield db.createCollection(col);
+
+	test.createIndex({hash: 1, ts: 1});
+	test.createIndex({expires: 1}, {expireAfterSeconds: 0});
+
+	var hash = '123';
+	var now = Date.now();
+	var ts = Math.trunc(now/1000);
+
+	var promises = new Array(20)
+		.join(',')
+		.split(',')
+		.map(function () {
+
+			return test.findOneAndUpdate(
+				{hash: hash, ts: ts},
+				{
+					$set: {expires: new Date(now + 60 * 1000)},
+					$inc: {count: 1}
+				},
+				{upsert: true}
+			);
+		});
+
+	yield promises;
+
+	var docs = yield test.find({}, {count: 1, _id: 0}).toArray();
+	console.log(docs);
+
+	process.exit();
+});
---
+++ mongodb.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mongod --config mongod.conf
